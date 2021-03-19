7a09d4198ad7ced02e3991692d84c717a891a002
Clay Heaton <ccheaton@gmail.com>
2021-03-19 17:34:17 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,2 @@
+# p5-playground
+# p5-playground
---
+++ p5js/empty-example/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(640, 360);
+}
+
+function draw() {
+  background(255);
+  fill(0,0,255);
+  ellipse(56, 46, 55, 55);
+}
